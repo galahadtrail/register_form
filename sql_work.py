b6c5130c4a5d3406fca9f80638d7cad102badbb7
galahadtrail <zzzzrzzzz@yandex.ru>
2022-10-04 19:36:35 +0300

    continue working with create_table function

--- sql_work.py
+++ sql_work.py
@@ -1,9 +1,17 @@
 """This module work with sql_lite database"""
 import os
 import sqlite3
+from pathlib import Path
+
+DB_FILENAME = "guests_database.db"
+
 
 def creating_table():
     basic_query = """
     CREATE TABLE Guests
     ( Id INT, Login STRING, Passwd STRING)
     """
+    if not os.path.exists(DB_FILENAME):
+        pth = Path(DB_FILENAME)
+        pth.touch(exist_ok=True)
+    connection = sqlite3.connect(DB_FILENAME)
