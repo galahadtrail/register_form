ec4d3d71491dcd00a9824f35a4bc2f7a4757d9da
galahadtrail <zzzzrzzzz@yandex.ru>
2022-10-04 19:20:45 +0300

    creating table function add

--- sql_work.py
+++ sql_work.py
@@ -1,1 +1,9 @@
 """This module work with sql_lite database"""
+import os
+import sqlite3
+
+def creating_table():
+    basic_query = """
+    CREATE TABLE Guests
+    ( Id INT, Login STRING, Passwd STRING)
+    """
